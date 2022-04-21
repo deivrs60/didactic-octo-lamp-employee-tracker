59039ca23c837f88828d89e6a231f170612608b1
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-21 13:44:10 -0700

    Added references to the role and employee tables. fixed syntax errors that did not allow schema.sql to properly run. upon execution

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+
+/* Pretty self explanatory - 
+If the table exists, it deletes it and then creates it again in order
+to avoid duplicating data 
+*/
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
@@ -12,13 +17,20 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT,
+
+    -- create the REFERENCE to the department ID, as per the module! */
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee ( 
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT
+    role_id INT,
+    manager_id INT,
+
+    -- creating the reference for both role_id and manager_id
+     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+     CONSTRAINT fk_managers FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+
+-- populate the department table with this seed data
 INSERT INTO department (name)
 VALUES
     ('TECHNOLOGY'),
@@ -5,16 +7,17 @@ VALUES
     ('RETAIL'),
     ('CORPORATE');
 
-
+--  populate the role table with this seed data
 INSERT INTO role (title, salary, department_id)
     VALUES 
-        ('TECHNOLOGY DIVISION LEAD', 17500000, 1 ),
+        ('TECHNOLOGY DIVISION LEAD', 17500000, 1),
         ('HEAD OF HUMAN RESOURCES', 12500000, 2),
         ('CHIEF FINANCIAL OFFICER', 15000000, 3),
         ('CHIEF OPERATIONS OFFICER', 20000000, 4),
         ('HEAD OF RECRUITING', 13000000, 5);
 
 
+-- populate the employee data with this seed data 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES  
         ('Dev', 'Deiv', 1, null),
